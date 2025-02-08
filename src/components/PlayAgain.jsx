3f7cb75a85a6e558e6619910903e871d6ad0ceac
Marek K <marek.krama@gmail.com>
2025-02-08 12:33:30 +0100

    techanical fix

--- src/components/PlayAgain.jsx
+++ src/components/PlayAgain.jsx
@@ -1,7 +1,18 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 export default function PlayAgain() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div>
-      <button onClick={() => window.location.reload()} className="play-again">
+      <button
+        onClick={() => {
+          navigate("/");
+          setTimeout(() => navigate(location.pathname), 0);
+        }}
+        className="play-again"
+      >
         Chcesz zagrać ponownie?
       </button>
     </div>
